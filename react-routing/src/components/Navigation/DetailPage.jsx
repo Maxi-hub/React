ed01b224ca83733a64d01ff93e8507b9f46d0902
Emma <em.lebedeva85@gmail.com>
2024-09-30 20:33:08 +0300

    Added variant with Link

--- react-routing/src/components/Navigation/DetailPage.jsx
+++ react-routing/src/components/Navigation/DetailPage.jsx
@@ -1,22 +1,25 @@
-import { useNavigate, useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+// import { Link, useNavigate, useParams } from "react-router-dom";
 import s from "../../App.module.css";
 import uuid from 'react-uuid';
 
 
 export const DetailPage = ({ items }) => {
     const { itemID } = useParams();
-    const navigate = useNavigate();
+    // const navigate = useNavigate();
     const item = items.find(item => item.id === parseInt(itemID, 10));
 
-    const goBack = () => {
-        navigate('/items');
-    }
+    // const goBack = () => {
+    //     navigate('/items'); 
+    // };
+
+    // В DetailPage для кнопки возврата можно использовать стандартную ссылку (<Link>), чтобы использовать функционал маршрутизации, встроенный в react-router, вместо вызова navigate().
 
     return (
         <div className={s.textBox}>
-            <button className={s.button} onClick={goBack}>Вернуться к анекдотам</button>
+            <Link className={s.itemLink} to="/items">Вернуться к анекдотам</Link>
+            {/* <button className={s.button} onClick={goBack()}>Вернуться к анекдотам</button> */}
             <ul>{(item.content).map(joke => <li key={uuid()} className={s.content}>{joke}</li>)}</ul>
-
         </div>
     )
 }
